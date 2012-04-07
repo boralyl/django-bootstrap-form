bee93dea4150cef67708b369234924e7ac91f312
juanpex <jpma55@gmail.com>
2012-04-07 18:40:59 -0300

    PEP8 and rename templatetag in compatibility with django-bostrapped

--- bootstrapform/templatetags/bootstrapform.py
+++ bootstrapform/templatetags/bootstrapform.py
@@ -4,8 +4,9 @@ from django import template
 
 register = template.Library()
 
+
 @register.filter
-def bootstrap(element):
+def bootstrapform(element):
     element_type = element.__class__.__name__.lower()
     if element_type == 'boundfield':
         template = get_template("bootstrapform/field.html")
@@ -13,8 +14,9 @@ def bootstrap(element):
     else:
         template = get_template("bootstrapform/form.html")
         context = Context({'form': element})
-        
+
     return template.render(context)
+
 
 @register.filter
 def is_checkbox(field):
