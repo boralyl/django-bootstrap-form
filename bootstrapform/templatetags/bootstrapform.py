a398d582f27f06eac38d4073769e16dbcef8b388
Aaron <aaron@little-one.(none)>
2012-04-14 13:54:33 -0700

    Added the ability add a css class as an argument to the filter for any form elements.

--- bootstrapform/templatetags/bootstrapform.py
+++ bootstrapform/templatetags/bootstrapform.py
@@ -1,16 +1,21 @@
+import re
+
+from django import template
 from django.template import Context
 from django.template.loader import get_template
-from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+FORM_ELEMENT_RE = re.compile(r'(<(input|textarea|select)\s)')
+
 
 @register.filter
-def bootstrapform(element):
+def bootstrapform(element, css_class=""):
     element_type = element.__class__.__name__.lower()
     if element_type == 'boundfield':
         template = get_template("bootstrapform/field.html")
-        context = Context({'field': element})
+        context = Context({'field': element, 'css_class': css_class})
     else:
         template = get_template("bootstrapform/form.html")
         context = Context({'form': element})
@@ -26,3 +31,18 @@ def is_checkbox(field):
 @register.filter
 def is_radio(field):
     return field.field.widget.__class__.__name__.lower() == "radioselect"
+
+
+@register.filter
+def add_class(value, css_class):
+    """
+    Adds a css class to any input, textarea, or select elements
+    """
+    string = unicode(value)
+    match = FORM_ELEMENT_RE.search(string)
+    css_class = "class='%s' " % (css_class, )
+
+    if match:
+        return mark_safe(string.replace(match.group(),
+            match.group() + css_class))
+    return value
